73d5a5dc69c4a9ad19455b5369a8f0d7df84a931
TatsianaKedrova <Baletrot@gmail.com>
2021-11-22 12:29:29 +0200

    feat(A-11): initial variables in js

---
+++ main.js
@@ -0,0 +1,11 @@
+const musicContainer = document.querySelector('.music-container');
+const playBtn = document.querySelector('#play');
+const nextBtn = document.querySelector('#next');
+const prevBtn = document.querySelector('#prev');
+const progress = document.querySelector('.progress');
+const audio = document.querySelector('#audio');
+const progressContainer = document.querySelector('.progress-container');
+const songTitle = document.querySelector('#title');
+const cover = document.querySelector('#cover');
+
+
