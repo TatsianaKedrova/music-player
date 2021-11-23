90b5dddacd9cf258aaa6a24e343eab731a7ef427
TatsianaKedrova <Baletrot@gmail.com>
2021-11-23 09:39:07 +0200

    feat(A-11): final change

--- main.js
+++ main.js
@@ -27,17 +27,53 @@ function playSong() {
   musicContainer.classList.add("play");
   playBtn.querySelector("i.fas").classList.remove("fa-play");
   playBtn.querySelector("i.fas").classList.add("fa-pause");
+
+  audio.play();
 }
 
 function pauseSong() {
   musicContainer.classList.remove("play");
   playBtn.querySelector("i.fas").classList.remove("fa-pause");
   playBtn.querySelector("i.fas").classList.add("fa-play");
+
+  audio.pause();
+}
+
+function prevSong() {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  console.log(songIndex);
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+function nextSong() {
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+  console.log(songIndex);
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+function updateProgress(e) {
+  const {duration, currentTime} = e.target;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+function setProgress(e) {
+ const width = this.clientWidth;
+ const clickX = e.offsetX;
+ console.log("offsetX: ", clickX);
 }
 
 //event listeners
 playBtn.addEventListener("click", () => {
-  const isPlaying = musicContainer.classList.contains(".play");
+  const isPlaying = musicContainer.classList.contains("play");
 
   if (isPlaying) {
     pauseSong();
@@ -45,3 +81,14 @@ playBtn.addEventListener("click", () => {
     playSong();
   }
 });
+
+//change song events
+prevBtn.addEventListener("click", prevSong);
+
+nextBtn.addEventListener("click", nextSong);
+
+audio.addEventListener('timeupdate', updateProgress)
+
+progressContainer.addEventListener('click', setProgress)
+
+
